c1cdff196191206a1d4b50f1761f44eda5ea8c51
wju784 <35173727+wju784@users.noreply.github.com>
2021-10-07 08:27:26 -0400

    Update exo.ps1

--- exo.ps1
+++ exo.ps1
@@ -1,2 +1,1 @@
-start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command "Invoke-WebRequest 'https://raw.githubusercontent.com/wju784/O365-Master/main/exo.ps1' -OutFile 'exo.ps1' -UseBasicParsing; .\exo.ps1"; 
-read-host "press enter"'
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass 
